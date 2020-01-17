155e3d03566ccbd98b2c70bb7a2ed5db95bf4c98
BrokenWrench2003 <43032452+BrokenWrench2003@users.noreply.github.com>
2020-01-17 09:07:09 +0200

    working on Q1

--- src/term1/Question1.java
+++ src/term1/Question1.java
@@ -1,10 +1,21 @@
 package term1;
 
+import java.util.Scanner;
+
 public class Question1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		//declarations
+		int number;
+		System.out.println("Please enter a number between 1 and 10:");
+		number = sc.nextInt();
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(i);
+			if(i % number == 0) {
+				System.out.println("*");
+			}
+		};
 	}
 
 }
